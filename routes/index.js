770a7901e39c71b88694748c7b01a6ad9403d734
sourabhagrawal23 <sourabhkhs23@gmail.com>
2020-09-20 13:12:28 +0530

    API Completed with both Get and Post endpoints

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,29 @@ const express = require('express')
 
 const router = express.Router()
 
+
+//Bringing Our Model
+
+const Url = require('../models/Url')
+
+// @route   GET /:code
+// @desc    Redirect to long/original URL
+router.get('/:code', async (req, res) => {
+    try {
+        const url = await Url.findOne({
+            urlCode: req.params.code
+        });
+
+        if (url) {
+            return res.redirect(url.longUrl);
+        } else {
+            return res.status(404).json('No Url Found')
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json('Server error');
+    }
+
+}) //: beacuse it is a parameter
+
 module.exports = router;
